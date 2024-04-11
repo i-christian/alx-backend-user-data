3549fef3fb0dcc7ee22eb5b200818bf40dadbefc
Christian <111076892+i-christian@users.noreply.github.com>
2024-04-11 23:37:21 +0200

    add filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+"""
+Module for handling Personal Data
+"""
+
+from typing import List
+import re
+import logging
+from os import environ
+import mysql.connector
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Obfuscates sensitive data fields in a log message.
+
+    Args:
+        fields (List[str]): A list of sensitive data fields to obfuscate.
+        redaction (str): The string to replace the sensitive data with.
+        message (str): The log message containing sensitive data.
+        separator (str): Used to delimit the fields in the log message.
+
+    Returns:
+        str: The log message with sensitive data obfuscated.
+    """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """
+    Retrieves a Logger object configured to handle Personal Data logging.
+
+    Returns:
+        logging.Logger: A Logger object configured for Personal Data logging.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Retrieves a connection to a MySQL database for Personal Data handling.
+
+    Returns:
+        mysql.connector.connection.MySQLConnection: A connector to database.
+    """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
+
+
+def main():
+    """
+    Retrieves a database connection and
+    displays filtered rows from the users table.
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    field_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
+    for row in cursor:
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
+        logger.info(str_row.strip())
+
+    cursor.close()
+    db.close()
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Formatter class for redacting sensitive data in log records.
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initializes the RedactingFormatter instance.
+
+        Args:
+            fields (List[str]): A list of sensitive data fields to redact.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats log records while redacting sensitive data.
+
+        Args:
+            record (logging.LogRecord): The log record to format.
+
+        Returns:
+            str: The formatted log message with sensitive data redacted.
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
+if __name__ == '__main__':
+    main()
